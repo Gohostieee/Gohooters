5d86b5a581f201ac3b9d359e5d0c7c9b2c483949
gohostieee <joshua32eb@gmail.com>
2022-07-20 06:40:32 -0400

    translating api functions from sql to firestore

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ import 'dotenv/config'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional 
+console.log({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+})
 const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
@@ -69,8 +78,48 @@ app.get('/api/accounts/signup', async (req,res)=>{
   }
   
 })
-app.post('/user/update', (req,res)=>{
-  const query = con.format("update userLogins set about = ? where name = ? and pass = ?",[utf8Enc(req.body.newInfo),req.body.username,hash256(req.body.password)])
+
+app.post('/user/folders/post/folder',async (req,res)=>{
+  let docSnap = query(userRef,where("name","==",req.body.name,"and","pass","==",hash256(req.body.pass)))
+  docSnap = await getDocs(docSnap)   
+  if(!docSnap.empty){
+      	const userFolderRef = collection(db, "users", docSnap.docs[0].id, "folders");
+      	const folderExists = query(userFolderRef,where("name","==",req.body.name,"and","pass","==",hash256(req.body.pass)))
+      	if(docSnap.empty){
+      		await setDoc(doc(db,'users', docSnap.docs[0].id, 'folders', req.body.folder), {
+				type:"folder",
+				level:req.body.currLevel,
+				parent:req.body.parent
+			});
+  			res.send({update:'passed'})
+
+      	}
+    	
+  return
+
+
+  }
+  	res.send({update:'userNotFound'})
+
+
+
+
+})
+app.post('/user/update',async (req,res)=>{
+  let docSnap = query(userRef,where("name","==",req.body.username,"and","pass","==",hash256(req.body.password)))
+  docSnap = await getDocs(docSnap)   
+  console.log(docSnap.docs[0].id)
+  if(!docSnap.empty){
+    	const userFolderRef = collection(db, "users", docSnap.docs[0].id, "folders");
+      
+
+  		res.send({update:'passed'})
+  		return
+
+
+  }
+  res.send({update:"userNotFound"})
+ /* const query = con.format("update userLogins set about = ? where name = ? and pass = ?",[utf8Enc(req.body.newInfo),req.body.username,hash256(req.body.password)])
   con.query(query,(error,response)=>{
         console.log(error,response,query)
         console.log(response['changedRows'])
@@ -86,22 +135,43 @@ app.post('/user/update', (req,res)=>{
           res.send({"update":"userNotFound"})
           return;
         }
-  })
+  })*/
 
 })
 //Yada Yada Yada, I have written this a million times(Fucking up the database  I gotta rewrite this😔, I'm the creator/ ONLY ENGINEER.
-app.get('/user/getInfo/Master', (req,res)=>{
-    const query = con.format("select * from userLogins where name = ? and pass = ?",[req.query['user'],hash256(req.query['pass'])])
-    con.query(query, (error,response)=>{  
-      response[0]['about']=utf8Dec(response[0]['about']);
-      res.send(response[0]); 
-    })
+app.get('/user/getInfo/Master',async (req,res)=>{
+
+  let docSnap = query(userRef,where("name","==",req.query['user'], "and", "pass", "==",hash256(req.query["pass"])))
+  docSnap = await getDocs(docSnap)   
+  if(!docSnap.empty){
+    res.send(docSnap.docs[0].data())
+    return;
+  }
+
+  res.send("userNotFound")
 }) 
-app.get('/user/folders/get',(req,res)=>{
-  const query = con.format("select * from userFolders where owner = ?",req.query['owner'])
-  con.query(query,(error, response)=>{
-    res.send(response)
-  })
+
+  
+
+app.get('/user/folders/get',async (req,res)=>{
+  let docSnap = query(userRef,where("name","==",req.query['owner']))
+  docSnap = await getDocs(docSnap) 
+  let folderArr=[],tempArr={};
+  if(!docSnap.empty){
+      const userFolderRef = collection(db, "users", docSnap.docs[0].id, "folders");
+      docSnap = await getDocs(userFolderRef)
+      docSnap.docs.forEach(x=>{
+      tempArr=x.data()
+      tempArr['name']=x.id
+      folderArr.push(tempArr)
+    })
+    res.send(folderArr)
+    return;
+  }
+  
+  
+  res.send("userNotFound")
+  
 })  
 
 app.listen(3001, ()=> console.log('listening....'));
